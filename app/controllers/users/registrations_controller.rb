be470119ae2c790cd46e972a0f6e48570eb37f64
KazumaAbe <kazuma23.ginga999@gmail.com>
2016-02-08 19:16:06 +0900

    user 関係の機能：AvatarUploader の実装／edit 機能修正

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,38 @@
+
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  def cancel
+    super
+  end
+ 
+  def create
+    super
+  end
+ 
+  def new
+    super
+  end
+
+  def edit
+    super
+  end
+ 
+  def update
+    super
+  end
+ 
+  def destroy
+    super
+  end
+
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update).push(:nickname, :profile, :works, :avatar, :group)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
   has_many :movies
 
+  mount_uploader :avatar, AvatarUploader
+
 end
