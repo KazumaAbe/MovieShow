333e9211eb5960cb76c74f76000f083c7fb0dc97
KazumaAbe <kazuma23.ginga999@gmail.com>
2016-02-27 19:50:24 +0900

    if文を三項演算子に変更

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -12,11 +12,9 @@ class MovieController < ApplicationController
   end
 
   def create
-    if Movie.create(movie_params).valid?
-      redirect_to :root, success: '投稿が完了しました'
-    else
-      redirect_to :back, warning: '投稿できませんでした。タイトルとキャッチコピー、コンセプトを入力してください'
-    end
+    Movie.create(movie_params).valid? ?
+      ( redirect_to :root, success: '投稿が完了しました' ) :
+      ( redirect_to :back, warning: '投稿できませんでした。タイトルとキャッチコピー、コンセプトを入力してください' )
   end
 
   def show
@@ -27,20 +25,16 @@ class MovieController < ApplicationController
   end
 
   def update
-    if @movie.update(movie_params)
-      redirect_to :root, success: '更新しました'
-    else
-      redirect_to :back, warning: '更新できませんでした。タイトルとサムネイル画像を入力してください'
-    end
+    @movie.update(movie_params) ?
+      ( redirect_to :root, success: '更新しました' ) :
+      ( redirect_to :back, warning: '更新できませんでした。タイトルとサムネイル画像を入力してください' )
   end
 
   def destroy
-    if @movie.user_id == current_user.id
-      @movie.destroy
-      redirect_to :root, success: '作品を削除しました'
-    else
-      redirect_to :back, warning: '作品の削除ができませんでした、ログインした後に再度実行してください'
-    end
+    @movie.user_id == current_user.id ?
+      ( @movie.destroy
+      redirect_to :root, success: '作品を削除しました' ) :
+      ( redirect_to :back, warning: '作品の削除ができませんでした、ログインした後に再度実行してください' )
   end
 
   private
