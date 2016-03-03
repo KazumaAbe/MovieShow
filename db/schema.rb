5d17b8bc960bf0b65aff98ee4921d2eb4a97e6e4
KazumaAbe <kazuma23.ginga999@gmail.com>
2016-03-03 22:47:20 +0900

    likeモデル作成、データベース生成

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227155226) do
+ActiveRecord::Schema.define(version: 20160303064227) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "text",       limit: 255
+    t.integer  "user_id",    limit: 4
+    t.integer  "movie_id",   limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "likes", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
     t.integer  "movie_id",   limit: 4
     t.datetime "created_at"
