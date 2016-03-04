dda172efa84341fae2bada358e58cee0b6a478b4
KazumaAbe <kazuma23.ginga999@gmail.com>
2016-03-05 00:30:02 +0900

    ajaxが着火しなかった件の修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,12 @@
 class LikesController < ApplicationController
 
   def create
+    @movie = Movie.find(params[:movie_id])
     Like.create(like_params)
   end
 
   def destroy
+    @movie = Movie.find(params[:movie_id])
     like = Like.find_by(user_id: current_user.id, movie_id: params[:id])
     like.destroy if like.user_id == current_user.id
   end
