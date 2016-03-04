9b104175677b622333c2033b06eb95774e1657c8
KazumaAbe <kazuma23.ginga999@gmail.com>
2016-03-05 00:08:44 +0900

    likesコントローラ作成、create/destroyアクション定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+
+  def create
+    Like.create(like_params)
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, movie_id: params[:id])
+    like.destroy if like.user_id == current_user.id
+  end
+
+  private
+  def like_params
+    params.permit(:movie_id).merge(user_id: current_user.id)
+  end
+
 end
