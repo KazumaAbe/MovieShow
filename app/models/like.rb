5057480b3a6e8d6dd8f313abb5ecac2496e6fd74
KazumaAbe <kazuma23.ginga999@gmail.com>
2016-03-07 12:20:43 +0900

    三項演算子に書き換え

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,11 +4,8 @@ class Like < ActiveRecord::Base
   belongs_to :movie, counter_cache: true
 
   def like_exists(a_movie_id, a_user_id)
-    if user_signed_in?
-      self.exists?(movie_id: a_movie_id, user_id: a_user_id)
-    else
-      return false
-    end
+    if user_signed_in ?
+      ( self.exists?(movie_id: a_movie_id, user_id: a_user_id) ) : ( return false )
   end
 
 end
