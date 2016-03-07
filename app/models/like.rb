8bb63c3544484a8249e270657dc2dd9dc871ab1e
KazumaAbe <kazuma23.ginga999@gmail.com>
2016-03-07 11:24:43 +0900

    判定条件のメソッド化、ビューの修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,14 @@
 class Like < ActiveRecord::Base
 
-belongs_to :user
-belongs_to :movie, counter_cache: true
+  belongs_to :user
+  belongs_to :movie, counter_cache: true
+
+  def like_exists(a_movie_id, a_user_id)
+    if user_signed_in?
+      self.exists?(movie_id: a_movie_id, user_id: a_user_id)
+    else
+      return false
+    end
+  end
 
 end
