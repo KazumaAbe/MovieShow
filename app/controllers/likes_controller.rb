6b64a7d9d046756779219cdf59ddeb8562c70514
KazumaAbe <kazuma23.ginga999@gmail.com>
2016-03-07 11:24:13 +0900

    before_action定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,12 @@
 class LikesController < ApplicationController
 
+  before_action :find_movie, only: [:create, :destroy]
+
   def create
-    @movie = Movie.find(params[:movie_id])
     Like.create(like_params)
   end
 
   def destroy
-    @movie = Movie.find(params[:movie_id])
     like = Like.find_by(user_id: current_user.id, movie_id: params[:id])
     like.destroy if like.user_id == current_user.id
   end
@@ -14,6 +14,10 @@ class LikesController < ApplicationController
   private
   def like_params
     params.permit(:movie_id).merge(user_id: current_user.id)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:movie_id])
   end
 
 end
