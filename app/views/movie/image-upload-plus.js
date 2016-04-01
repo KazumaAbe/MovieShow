0c2fb538035c156b95d2cb7ad45a1ce9491ff60b
KazumaAbe <kazuma23.ginga999@gmail.com>
2016-04-01 22:55:40 +0900

    JSファイルの追加

---
+++ app/views/movie/image-upload-plus.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.image-upload-plus').click(function(){
+    $document.getElementById('plus-button').innerHTML = "<div class='col-md-12'>
+      <%= f.fields_for :thumbnails do |t| %>
+        <div class='cover-image-upload' id='upload3'
+          <%= t.file_field :title, class: 'thumbnail-field', id: 'thumb-3' %>
+          <div class='thumb-image'>
+            <%= t.hidden_field :status, value: 'sub' %>";
+      ('.image-upload-plus span').remove;
+  });
+});
